b72231e5f5b4654bde166b4c473518d2d80e57ec
Andrea Parenti <andrea.parenti@xfel.eu>
2020-01-07 18:18:10 +0100

    Done expectedParameters, updateOutputSchema and writeChannels

--- src/ImageSource.cc
+++ src/ImageSource.cc
@@ -1,7 +1,7 @@
 /*
  * Author: <parenti>
  *
- * Created on January, 2020, 05:11 PM
+ * Created on January  7, 2020, 05:11 PM
  *
  * Copyright (c) European XFEL GmbH Hamburg. All rights reserved.
  */
@@ -18,6 +18,29 @@ namespace karabo {
     KARABO_REGISTER_FOR_CONFIGURATION(BaseDevice, Device<>, ImageSource)
 
     void ImageSource::expectedParameters(Schema& expected) {
+        Schema data;
+
+        NODE_ELEMENT(data).key("data")
+                .displayedName("Data")
+                .setDaqDataType(DaqDataType::TRAIN)
+                .commit();
+
+        IMAGEDATA(data).key("data.image")
+                .displayedName("Image")
+                .commit();
+
+        OUTPUT_CHANNEL(expected).key("output")
+                .displayedName("Output")
+                .dataSchema(data)
+                .commit();
+
+        // Second output channel for the DAQ
+        OUTPUT_CHANNEL(expected).key("daqOutput")
+                .displayedName("DAQ Output")
+                .dataSchema(data)
+                .commit();
+
+        // TODO: availableScenes
     }
 
 
@@ -28,11 +51,69 @@ namespace karabo {
     ImageSource::~ImageSource() {
     }
 
+    // TODO doc
+    void ImageSource::updateOutputSchema(const std::vector<int>& shape, const karabo::xms::EncodingType& encoding,
+            const karabo::util::Types::ReferenceType& kType) {
+        Schema schemaUpdate;
+        this->schema_update_helper(schemaUpdate, "output", "Output", shape, encoding, kType);
 
-    void ImageSource::preReconfigure(karabo::util::Hash& incomingReconfiguration) {
+        std::vector<int> daqShape = shape;
+        std::reverse(daqShape.begin(), daqShape.end()); // NB DAQ wants shape in CImg order, eg (width, height)
+        this->schema_update_helper(schemaUpdate, "daqOutput", "DAQ Output", daqShape, encoding, kType);
+
+        this->appendSchema(schemaUpdate);
     }
 
 
-    void ImageSource::postReconfigure() {
+    void ImageSource::schema_update_helper(karabo::util::Schema& schemaUpdate, const std::string& nodeKey,
+            const std::string& displayedName, const std::vector<int>& shape, const karabo::xms::EncodingType& encoding,
+            const karabo::util::Types::ReferenceType& kType) {
+        Schema dataSchema;
+        NODE_ELEMENT(dataSchema).key("data")
+                .displayedName("Data")
+                .setDaqDataType(DaqDataType::TRAIN)
+                .commit();
+
+        IMAGEDATA(dataSchema).key("data.image")
+                .displayedName(displayedName)
+                .setDimensions(karabo::util::toString(shape))
+                .setType(kType)
+                .setEncoding(encoding)
+                .commit();
+
+        OUTPUT_CHANNEL(schemaUpdate).key(nodeKey)
+                .displayedName(displayedName)
+                .dataSchema(dataSchema)
+                .commit();
+    }
+
+
+    // TODO docs
+    void ImageSource::writeChannels(karabo::util::NDArray& data, const karabo::util::Dims& binning,
+                const unsigned short bpp, const karabo::xms::EncodingType& encoding,
+                const karabo::util::Dims& roiOffsets, const karabo::util::Timestamp& timestamp) {
+
+        this->write_channel("output", data, binning, bpp, encoding, roiOffsets, timestamp);
+
+        // Reshaped image for DAQ
+        // NB DAQ wants shape in CImg order, e.g. (width, height)
+        Dims daqShape = data.getShape();
+        daqShape.reverse();
+        NDArray daqData = data;
+        daqData.setShape(daqShape);
+        this->write_channel("daqOutput", daqData, binning, bpp, encoding, roiOffsets, timestamp);
+    }
+
+
+    void ImageSource::write_channel(const std::string& nodeKey, karabo::util::NDArray& data, const karabo::util::Dims& binning,
+                const unsigned short bpp, const karabo::xms::EncodingType& encoding,
+                const karabo::util::Dims& roiOffsets, const karabo::util::Timestamp& timestamp) {
+
+        karabo::xms::ImageData imageData(data, encoding);
+        imageData.setBitsPerPixel(bpp);
+        imageData.setROIOffsets(roiOffsets);
+        imageData.setBinning(binning);
+
+        this->writeChannel(nodeKey, Hash("data.image", imageData), timestamp);
     }
 }
--- src/ImageSource.hh
+++ src/ImageSource.hh
@@ -1,7 +1,7 @@
 /*
  * Author: <parenti>
  *
- * Created on January, 2020, 05:11 PM
+ * Created on January  7, 2020, 05:11 PM
  *
  * Copyright (c) European XFEL GmbH Hamburg. All rights reserved.
  */
@@ -20,56 +20,38 @@ namespace karabo {
 
     public:
 
-        // Add reflection information and Karabo framework compatibility to this class
-        KARABO_CLASSINFO(ImageSource, "ImageSource", "2.0")
+        KARABO_CLASSINFO(ImageSource, "ImageSource", "2.7")
 
-        /**
-         * Necessary method as part of the factory/configuration system
-         * @param expected Will contain a description of expected parameters for this device
-         */
         static void expectedParameters(karabo::util::Schema& expected);
 
-        /**
-         * Constructor providing the initial configuration in form of a Hash object.
-         * If this class is constructed using the configuration system the Hash object will
-         * already be validated using the information of the expectedParameters function.
-         * The configuration is provided in a key/value fashion.
-         */
         ImageSource(const karabo::util::Hash& config);
 
-        /**
-         * The destructor will be called in case the device gets killed
-         */
         virtual ~ImageSource();
 
-        /**
-         * This function acts as a hook and is called after an reconfiguration request was received,
-         * but BEFORE this reconfiguration request is actually merged into this device's state.
-         *
-         * The reconfiguration information is contained in the Hash object provided as an argument.
-         * You have a chance to change the content of this Hash before it is merged into the device's current state.
-         *
-         * NOTE: (a) The incomingReconfiguration was validated before
-         *       (b) If you do not need to handle the reconfigured data, there is no need to implement this function.
-         *           The reconfiguration will automatically be applied to the current state.
-         * @param incomingReconfiguration The reconfiguration information as was triggered externally
-         */
         virtual void preReconfigure(karabo::util::Hash& incomingReconfiguration);
 
+    protected:
 
-        /**
-         * This function acts as a hook and is called after an reconfiguration request was received,
-         * and AFTER this reconfiguration request got merged into this device's current state.
-         * You may access any (updated or not) parameters using the usual getters and setters.
-         * @code
-         * int i = get<int>("myParam");
-         * @endcode
-         */
-        virtual void postReconfigure();
+        void updateOutputSchema(const std::vector<int>& shape,
+                const karabo::xms::EncodingType& encoding,
+                const karabo::util::Types::ReferenceType& kType);
 
+        void writeChannels(karabo::util::NDArray& data, const karabo::util::Dims& binning,
+                const unsigned short bpp, const karabo::xms::EncodingType& encoding,
+                const karabo::util::Dims& roiOffsets, const karabo::util::Timestamp& timestamp);
 
     private:
 
+        void schema_update_helper(karabo::util::Schema& schemaUpdate,
+                const std::string& nodeKey, const std::string& displayedName,
+                const std::vector<int>& shape, const karabo::xms::EncodingType& encoding,
+                const karabo::util::Types::ReferenceType& kType);
+
+        void write_channel(const std::string& nodeKey, karabo::util::NDArray& data, const karabo::util::Dims& binning,
+                const unsigned short bpp, const karabo::xms::EncodingType& encoding,
+                const karabo::util::Dims& roiOffsets, const karabo::util::Timestamp& timestamp);
+
+        // TODO implement scene map protocol
     };
 }
 
